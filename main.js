78ca4e09fe2ba9a3d416a3b07a1fbd49540e81f1
Filippo <filippotabbi23@gmail.com>
2021-03-17 17:47:52 +0100

    esercizio base terminato (!css)

---
+++ main.js
@@ -0,0 +1,33 @@
+  $(document).ready(function () {
+    $('#add').click(function () {
+      var todoTitle = $('#todoTitle').val();
+      $('#todoTitle').val('');
+
+      var template = $('#todo li').clone();
+      template.children('span.title').text(todoTitle);
+
+      template.children('span.delete').click(function () {
+        $(this).parent().remove();
+      });
+
+      template.children('span.edit').click(function () {
+        var titleDOM = $(this).siblings('span.title');
+        var title = titleDOM.text();
+        titleDOM.hide();
+
+        $(this).siblings('input').val(title).show();
+
+      });
+
+        template.children('input').keyup(function (e) {
+          if (e.which == 13) {
+            var title = $(this).val();
+            $(this).hide();
+            $(this).siblings('span.title').text(title).show();
+
+          }
+        });
+
+      $('#todos').append(template);
+    });
+  });
